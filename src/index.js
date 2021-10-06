4ea07c8c8a714e0c6bbc08377b68641ee9df41fd
Subin Sebastian <subin.sebastian457@gmail.com>
2021-10-06 14:34:12 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import * as env from '../environments';
-
-const foo = 'bar';
-console.log(foo);
-const abc = ['a', 'b'];
-const pqr = ['a', 'b'];
-const defg = pqr;
-const xyz = pqr;
-console.log(abc);
-
-const lmn = pqr;
+const abc = 2;
